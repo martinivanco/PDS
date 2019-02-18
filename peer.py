b8f907b50a49308dbf10d0b9f7b0f0163ae3ae76
Martin Ivanco <ivancom.fr@gmail.com>
2019-02-18 20:49:26 +0100

    Updated peer - added hello thread

--- peer.py
+++ peer.py
@@ -1,24 +1,99 @@
+import sys
+import time
+import argparse
+import threading
+import ipaddress
 import tools
 
+class HelloThread(threading.Thread):
+    def __init__(self, packet, node):
+        super(HelloThread, self).__init__()
+        self.packet = packet
+        self.node_ip = node[0]
+        self.node_port = node[1]
+        self.stop_f = False
+        
+    def run(self):
+        while not self.stop_f:
+            self.packet["txid"] = tools.generate_txid()
+            # bencode and send it
+            for i in range(10):
+                if self.stop_f:
+                    break
+                time.sleep(1)
+        self.packet["txid"] = tools.generate_txid()
+        self.packet["ipv4"] = "0.0.0.0"
+        self.packet["port"] = 0
+        # bencode and send it
+    
+    def stop(self):
+        self.stop_f = True
+
 class PeerDaemon:
-    def __init__(self):
-        pass
+    def __init__(self, info):
+        self.info = info
+        self.hello_packet = {
+            "type": "hello",
+            "txid": 0,
+            "username": info.username,
+            "ipv4": str(info.chat_ipv4),
+            "port": info.chat_port
+        }
+        self.peers = []
+        self.hello_thread = HelloThread(self.hello_packet, (self.info.reg_ipv4, self.info.reg_port))
+        self.hello_thread.start()
 
     def send_message(self, sender, recipient, message):
         print(recipient + ": " + message)
         return True
 
     def update_peer_list(self):
-        print("getlist")
+        # getlist
+        # for any in response not in peers append
+        # má rpc čakať?
         return True
 
     def get_peer_list(self):
-        print("peers")
-        return "this will be a list of peers"
+        return self.peers
     
     def change_reg_node(self, ip_address, port):
-        print("reconnect")
+        self.hello_thread.stop()
+        self.info.reg_ipv4 = ipaddress.ip_address(ip_address)
+        self.info.reg_port = port
+        self.hello_thread.join()
+        self.hello_thread = HelloThread(self.hello_packet, (self.info.reg_ipv4, self.info.reg_port))
+        self.hello_thread.start()
         return True
+    
+    def finish(self):
+        self.hello_thread.stop()
+        self.hello_thread.join()
 
+def main():
+    parser = argparse.ArgumentParser(description="Peer Daemon for PDS18 P2P Chat")
+    parser.add_argument("-i", "--id", type = tools.id_check, required = True, metavar = "<id>",
+        help = "unique id of the peer")
+    parser.add_argument("-u", "--username", required = True, metavar = "<username>",
+        help = "unique username of the peer")
 
-tools.run_server(PeerDaemon())
+    parser.add_argument("-ci", "--chat-ipv4", type = tools.ip_check, required = True, metavar = "<ip addr>",
+        help = "IP address on which the chat peer should listen for messages")
+    parser.add_argument("-cp", "--chat-port", type = tools.port_check, required = True, metavar = "<port>",
+        help = "port on which the chat peer should listen for messages")
+
+    parser.add_argument("-ri", "--reg-ipv4", type = tools.ip_check, required = True, metavar = "<ip addr>",
+        help = "IP address of registration node")
+    parser.add_argument("-rp", "--reg-port", type = tools.port_check, required = True, metavar = "<port>",
+        help = "port on which registration node listens for connections")
+    args = parser.parse_args()
+    daemon = PeerDaemon(args)
+    try:
+        tools.run_server(daemon, args.id % 19991 + 10000)
+    except KeyboardInterrupt:
+        tools.err_print("\nStopping daemon...")
+        daemon.finish()
+        tools.err_print("Bye.")
+
+if __name__ == "__main__":
+    main()
+
--- tools.py
+++ tools.py
@@ -1,3 +1,7 @@
+import sys
+import argparse
+import uuid
+import ipaddress
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 from xmlrpc.client import ServerProxy
@@ -5,10 +9,46 @@ from xmlrpc.client import ServerProxy
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-def run_server(daemon):
-    with SimpleXMLRPCServer(('localhost', 8000), requestHandler = RequestHandler) as server:
+def run_server(daemon, port):
+    with SimpleXMLRPCServer(('localhost', port), requestHandler = RequestHandler) as server:
         server.register_instance(daemon)
         server.serve_forever()
 
-def get_stub():
-    return ServerProxy('http://localhost:8000')
+def get_stub(port):
+    return ServerProxy('http://localhost:' + str(port))
+
+def id_check(value):
+    msg = "%r is not a valid id - must be a positive integer" % value
+    try:
+        id_val = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(msg)
+    if id_val <= 0:
+        raise argparse.ArgumentTypeError(msg)
+    return id_val
+
+def ip_check(value):
+    msg = "%r is not a valid IPv4 address" % value
+    try:
+        ip_val = ipaddress.ip_address(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(msg)
+    if ip_val.version != 4:
+        raise argparse.ArgumentTypeError(msg)
+    return ip_val
+
+def port_check(value):
+    msg = "%r is not a valid port" % value
+    try:
+        port_val = int(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(msg)
+    if port_val < 0 or port_val > (2**16 - 1):
+        raise argparse.ArgumentTypeError(msg)
+    return port_val
+
+def generate_txid():
+    return uuid.uuid4().int & 0xffff
+
+def err_print(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
